6fb9982cd5f0b02b19d3f65358071d0858387763
gangothri.a <gangothri.a@GSPS2P9S542>
2025-01-25 19:50:12 +0530

    Add project files.

---
+++ MVCController2/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCController2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Blogs()
+        {
+            return Content("Hi");
+        }
+        public ViewResult About()
+        {
+            return View();
+        }
+        public ContentResult Contact()
+        {
+            return Content("Hello");
+        }
+        public RedirectResult redirect()
+        {
+            return Redirect("https://www.google.com/");
+        }
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+        public ActionResult RedirectWithId()
+        {
+            return RedirectToRoute(new { controller = "Home", action = "Details", id = 1 });
+        }
+        public FileResult Index2()
+        {
+            return File(Url.Content(@"C:\Users\gangothri.a\Downloads\ADO.Net.txt"), "text/plain");
+        }
+        public FileResult Index3()
+        {
+            return File(Url.Content("~/Files/abc.txt"), "text/plain");
+        }
+        public JsonResult Getemp(int id)
+        {
+            var emp = new { Id = id, name = "gangu", sal = 12345 };
+            return Json(emp, JsonRequestBehavior.AllowGet);
+        }
+        public JavaScriptResult msg()
+        {
+            string msg = "Hello";
+            return JavaScript(msg);
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult loginlist()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Registerhelper()
+        {
+            ViewBag.States =new SelectList(new List<string>
+            {
+                "Select",
+                "Telangana",
+                "Andhra Pradesh",
+                "Kerala",
+                "Tamil Nadu"
+            });
+            return View();
+        }
+        
+    }
+}
---
+++ MVCController2/Controllers/RegisterController.cs
@@ -0,0 +1,99 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+
+//namespace MVCController2.Controllers
+//{
+//    public class RegisterController : Controller
+//    {
+
+//        public ActionResult Formcontroller()
+//        {
+//            ViewBag.states = new SelectList(new List<string> 
+//            { 
+//                "Select",
+//                "Andhra Pradesh", 
+//                "Telangana", 
+//                "Kerala" 
+//            });
+//            return View();
+//        }
+//        [HttpPost]
+//        public ActionResult Formcontroller(FormCollection fobj)
+//        {
+//            Response.Write("name:" + fobj["namelabel"]);
+//            Response.Write("username:" + fobj["userlabel"]);
+//            Response.Write("mail:" + fobj["maillbl"]);
+//            Response.Write("pws:" + fobj["pwdlabel"]);
+//            Response.Write("gender:" + fobj["genderlbl"]);
+//            Response.Write("state:" + fobj["statelabel"]);
+//            //Response.Write("robot:" + fobj["check"] == "true, false");
+//            if (fobj["check"] == "true")
+//            {
+//                Response.Write("Registration successful.<br>");
+//            }
+//            else
+//            {
+//                Response.Write("Please confirm you are not a robot.<br>");
+//            }
+
+//            return View();
+//        }   
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace MVCController2.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Formcontroller
+        public ActionResult Formcontroller()
+        {
+            ViewBag.states = new SelectList(new List<string>
+            {
+                "Select",
+                "Andhra Pradesh",
+                "Telangana",
+                "Kerala"
+            });
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Formcontroller(FormCollection fobj)
+        {
+            string name = fobj["name"];
+            string username = fobj["uname"];
+            string email = fobj["mail"];
+            string password = fobj["pwd"];
+            string gender = fobj["gender"];
+            string state = fobj["statelabel"];
+            bool isRobot = fobj["checkbox"] == "true,false";
+
+            Response.Write($"Name: {name}<br>");
+            Response.Write($"Username: {username}<br>");
+            Response.Write($"Email: {email}<br>");
+            Response.Write($"Password: {password}<br>");
+            Response.Write($"Gender: {gender}<br>");
+            Response.Write($"State: {state}<br>");
+
+            if (isRobot)
+            {
+                Response.Write("Registration successful.<br>");
+            }
+            else
+            {
+                Response.Write("Please confirm you are not a robot.<br>");
+            }
+
+            return View();
+        }
+    }
+}
